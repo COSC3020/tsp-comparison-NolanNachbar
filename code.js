d058d052c927dd8419f3392e023e11ee8c022fc5
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-11-16 15:36:57 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,27 @@
+function mergesort(array) {
+    var subarraysize = 1;
+
+    while (subarraysize < array.length) {
+        for (var start = 0; start < array.length; start += 2 * subarraysize) {
+            var midsub = Math.min(start + subarraysize, array.length);
+            var endsub = Math.min(start + 2 * subarraysize, array.length);
+            
+            var a = start, b = midsub;
+            while (a < b && b < endsub) {
+                if (array[a] <= array[b]) {
+                    a++;
+                } else {
+                    let t = array[b];
+                    for (let k = b; k > a; k--) {
+                        array[k] = array[k - 1];
+                    }
+                    array[a] = t;
+                    a++;
+                    b++;
+                }
+            }
+        }
+        subarraysize *= 2;
+    }
+    return array;
+}
